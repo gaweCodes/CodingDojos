8e2eab6f1652c643333e37de93956e39d681d7eb
Gabriel Weibel <gabriel.weibel@hotmail.de>
2020-01-19 16:34:43 +0100

    Loops

---
+++ CodingDojos/FizzBuzz1_Loops/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FizzBuzz1_Loops
+{
+    internal class Program
+    {
+        private const int MaxValue = 100;
+        private const int ValueToPrintFizz = 5;
+        private const int ValueToPrintBuzz = 7;
+        private static void Main()
+        {
+            Solutijon1();
+            Solutijon2();
+        }
+        private static void Solutijon1()
+        {
+            Console.WriteLine("Solution 1");
+            for (var number = 1; number < MaxValue; number++)
+            {
+                if (IsFizzBuzz(number))
+                    Console.WriteLine($"{number}: fizzbuzz");
+                else if (IsFizz(number))
+                    Console.WriteLine($"{number}: fizz");
+                else if (IsBuzz(number))
+                    Console.WriteLine($"{number}: buzz");
+            }
+        }
+        private static void Solutijon2()
+        {
+            Console.WriteLine("Solution 2");
+            for (var number = 1; number < MaxValue; number++)
+            {
+                Console.Write($"{number}:");
+                if (IsFizz(number))
+                    Console.Write("fizz");
+                if (IsBuzz(number))
+                    Console.Write("buzz");
+                Console.WriteLine();
+            }
+        }
+        private static bool IsFizz(int number) => number % ValueToPrintFizz == 0;
+        private static bool IsBuzz(int number) => number % ValueToPrintBuzz == 0;
+        private static bool IsFizzBuzz(int number) => IsFizz(number) && IsBuzz(number);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CodingDojos
+
+## FizzBuss 1 Loops
+
+Write a program that prints the numbers from 1 to 100. For each number that is divisible by 5, "fizz" should be output and for each number that is divisible by 7, "buzz" should be output. If the number is divisible by both 7 and 5, "fizzbuzz" should be output. The modulo operator determines the rest of the division. Divisibility is thus easily achieved when the modulo operation (%, MOD) returns the remainder 0.
